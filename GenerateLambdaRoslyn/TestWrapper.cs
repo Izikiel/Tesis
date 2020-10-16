93a46f8eedb15b9ed9646a0efd271e973bf8e6bf
Ezequiel Gambaccini <ezgambac@microsoft.com>
2020-10-15 19:51:44 -0700

    Added iclass fixture handling

--- GenerateLambdaRoslyn/TestWrapper.cs
+++ GenerateLambdaRoslyn/TestWrapper.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Threading.Tasks;
-
+using Xunit;
 
 namespace GenerateLambdaRoslyn
 {
     public class TestWrapper
     {
         private readonly Type objectType;
-        private readonly object[] constructorArgs;
+        private object[] constructorArgs;
+        private readonly Type constructorArgsType;
         private readonly string methodName;
         private readonly object[] methodArgs;
         private readonly Type[] methodArgsTypes;
-        private object instance;
 
         public TestWrapper(Type objectType, string methodName, params object[] methodArgs)
         {
-            this.objectType = objectType; // TODO: Check if it's IClassFixture<T> type. If so, try to get T, construct it, and pass it to the obj constructor.
-            this.constructorArgs = null; // new object[] { };
+            this.objectType = objectType; // TODO: Check what to do regarding ICollectionFixture.
+            this.constructorArgsType = this.GetConstructorArgsType(objectType);
             this.methodName = methodName;
             this.methodArgs = methodArgs ?? new object[] { };
 
@@ -30,19 +30,39 @@ namespace GenerateLambdaRoslyn
             }
         }
 
+        private Type GetConstructorArgsType(Type objectType)
+        {
+            var iClassFixtureInstance = Array.Find(objectType.GetInterfaces(), i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IClassFixture<>));
+
+            if (iClassFixtureInstance is null)
+            {
+                return null;
+            }
+
+            return iClassFixtureInstance.GetGenericArguments()[0];
+        }
+
+        private object[] CreateConstructorArgs() => this.constructorArgsType is null ? null : new object[] { Activator.CreateInstance(this.constructorArgsType, Type.EmptyTypes) };
+
         public async Task Invoke()
         {
+            object instance = null;
+            object[] constructorArgs = null;
             try
             {
                 var methodToInvoke = this.objectType.GetMethod(this.methodName, this.methodArgsTypes);
 
-                this.instance = methodToInvoke.IsStatic ? null : Activator.CreateInstance(this.objectType, this.constructorArgs);
+                if (!methodToInvoke.IsStatic)
+                {
+                    constructorArgs = this.CreateConstructorArgs();
+                    instance = Activator.CreateInstance(this.objectType, constructorArgs);
+                }
 
-                await (Task)methodToInvoke.Invoke(this.instance, this.methodArgs);
+                await (Task)methodToInvoke.Invoke(instance, this.methodArgs);
             }
             finally
             {
-                if (this.instance is IDisposable disposable)
+                if (instance is IDisposable disposable)
                 {
                     disposable.Dispose();
                 }
@@ -50,6 +70,18 @@ namespace GenerateLambdaRoslyn
                 else if (instance is IAsyncDisposable asyncDisposable)
                 {
                     await asyncDisposable.DisposeAsync();
+                }
+
+                if (constructorArgs?.Length > 0)
+                {
+                    if (constructorArgs[0] is IDisposable disposableArg)
+                    {
+                        disposableArg.Dispose();
+                    }
+                    else if (constructorArgs[0] is IAsyncDisposable asyncDisposableArg)
+                    {
+                        await asyncDisposableArg.DisposeAsync();
+                    }
                 }
             }
         }
