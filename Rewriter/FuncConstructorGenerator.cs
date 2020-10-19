11a8bf45b14447ac80fdaba3687e503b371c4c31
Ezequiel Gambaccini <ezgambac@microsoft.com>
2020-10-19 01:34:48 -0700

    Removed old wrapper, moved func generator to rewriter

---
+++ Rewriter/FuncConstructorGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Rewriter
+{
+    public static class FuncConstructorGenerator
+    {
+        static Type[] FuncTypes { get; } = new Type[]
+        {
+            typeof(Func<>),
+            typeof(Func<,>),
+            typeof(Func<,,>),
+            typeof(Func<,,,>),
+            typeof(Func<,,,,>),
+            typeof(Func<,,,,,>),
+            typeof(Func<,,,,,,>),
+            typeof(Func<,,,,,,,>),
+            typeof(Func<,,,,,,,,>),
+            typeof(Func<,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,,,>),
+        };
+
+        public static Type InstatiatedFuncType(params object[] argsForConstructor)
+        {
+            var typeArguments = argsForConstructor?.Select(a => a.GetType()).ToArray() ?? Array.Empty<Type>();
+
+            return FuncConstructorGenerator.InstatiatedFuncType(typeArguments);
+        }
+
+        public static ConstructorInfo GetConstructorInfo(params object[] argsForConstructor)
+        {
+            var typeArguments = argsForConstructor?.Select(a => a.GetType()).ToArray() ?? Array.Empty<Type>();
+
+            return FuncConstructorGenerator.GetConstructorInfo(typeArguments);
+        }
+
+        public static Type InstatiatedFuncType(params Type[] argsForConstructor)
+        {
+            argsForConstructor ??= Array.Empty<Type>();
+
+            var funcType = FuncTypes[argsForConstructor.Length];
+
+            var typeArguments = argsForConstructor.Append(typeof(Task)).ToArray();
+
+            return funcType.MakeGenericType(typeArguments);
+        }
+
+        public static ConstructorInfo GetConstructorInfo(params Type[] argsForConstructor)
+        {
+            argsForConstructor ??= Array.Empty<Type>();
+
+            var funcType = FuncTypes[argsForConstructor.Length];
+
+            var typeArguments = argsForConstructor.Append(typeof(Task)).ToArray();
+
+            var instantiatedFuncType = funcType.MakeGenericType(typeArguments);
+
+            return instantiatedFuncType.GetConstructors()[0];
+        }
+    }
+}
