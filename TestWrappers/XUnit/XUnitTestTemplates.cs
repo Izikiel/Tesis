c39f8175823f6817bca96de28fe8c83121bde2b9
Ezequiel Gambaccini <ezgambac@microsoft.com>
2020-10-19 01:33:16 -0700

    New project to hold template and wrapper

---
+++ TestWrappers/XUnit/XUnitTestTemplates.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.Coyote;
+using Microsoft.Coyote.SystematicTesting;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestWrappers.XUnit
+{
+    public static class XUnitTestTemplates
+    {
+        public static void FactTemplate()
+        {
+            using var testWrapper = new XUnitTestWrapper(null, null, null);
+            XUnitTestTemplates.RunTestInCoyote(testWrapper.Invoke);
+        }
+
+        public static void TheoryTemplate(params object[] args)
+        {
+            using var testWrapper = new XUnitTestWrapper(null, null, args);
+            XUnitTestTemplates.RunTestInCoyote(testWrapper.Invoke);
+        }
+
+        public static void RunTestInCoyote(Func<Task> toRun)
+        {
+            var configuration = Configuration.Create().WithTestingIterations(1000).WithRandomStrategy();
+            var testingEngine = TestingEngine.Create(configuration, toRun);
+            testingEngine.Run();
+
+            var report = testingEngine.TestReport;
+            var bugCount = report.BugReports.Count;
+
+            if (bugCount > 0)
+            {
+                var reports = new string[bugCount];
+
+                report.BugReports.CopyTo(reports);
+
+                Assert.True(false, $"Test failed. Found {bugCount} bugs. Errors: {string.Join(",", reports)}");
+            }
+
+            Console.WriteLine("Test passed");
+        }
+    }
+}
---
+++ TestWrappers/XUnit/XUnitTestWrapper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestWrappers.XUnit
+{
+    public class XUnitTestWrapper : IDisposable
+    {
+        private readonly Type objectType;
+        private readonly Type[] constructorArgsTypes;
+        private readonly object[] constructorArgs;
+        private readonly string methodName;
+        private readonly object[] methodArgs;
+        private readonly Type[] methodArgsTypes;
+
+        public XUnitTestWrapper(Type objectType, string methodName, params object[] methodArgs)
+        {
+            this.objectType = objectType; // TODO: Check what to do regarding ICollectionFixture.
+            this.constructorArgsTypes = this.GetConstructorArgsTypes(objectType);
+            this.constructorArgs = this.CreateConstructorArgs();
+            this.methodName = methodName;
+            this.methodArgs = methodArgs;
+            this.methodArgsTypes = Type.EmptyTypes;
+
+            var argsLength = this.methodArgs?.Length ?? 0;
+            if (argsLength > 0)
+            {
+                this.methodArgsTypes = new Type[argsLength];
+
+                for (var i = 0; i < argsLength; i++)
+                {
+                    this.methodArgsTypes[i] = this.methodArgs[i].GetType();
+                }
+            }
+        }
+
+        private Type[] GetConstructorArgsTypes(Type objectType)
+        {
+            var iClassFixtureInstances = Array.FindAll(objectType.GetInterfaces(), i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IClassFixture<>));
+
+            return Array.ConvertAll(iClassFixtureInstances, instance => instance.GetGenericArguments()[0]);
+        }
+
+        private object[] CreateConstructorArgs()
+        {
+            if (this.constructorArgsTypes is null)
+            {
+                return null;
+            }
+            return Array.ConvertAll(this.constructorArgsTypes, typeInstance => Activator.CreateInstance(typeInstance, Type.EmptyTypes));
+        }
+
+        public async Task Invoke()
+        {
+            object instance = null;
+            try
+            {
+                var methodToInvoke = this.objectType.GetMethod(this.methodName, this.methodArgsTypes);
+
+                if (!methodToInvoke.IsStatic)
+                {
+                    instance = Activator.CreateInstance(this.objectType, this.constructorArgs);
+                }
+
+                await (Task)methodToInvoke.Invoke(instance, this.methodArgs);
+            }
+            finally
+            {
+                if (instance is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+                else if (instance is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < this.constructorArgs?.Length; i++)
+            {
+                if (this.constructorArgs[i] is IDisposable disposableArg)
+                {
+                    disposableArg.Dispose();
+                }
+            }
+        }
+    }
+}
